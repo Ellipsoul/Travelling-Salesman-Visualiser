0a168ea88342571e9eabb4b422a6bd1bfb9dab7e
Aron Teh <ewt17@ic.ac.uk>
2020-07-07 00:24:39 +0100

    Adding project title and algorithms dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 // import { MatSliderModule } from '@angular/material/slider';
 // import { MatButtonToggleModule } from '@angular/material/button-toggle';
 // import { MatGridListModule } from '@angular/material/grid-list';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopbarComponent } from './topbar/topbar.component';
-import { GridComponent } from './grid/grid.component'
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +21,14 @@ import { GridComponent } from './grid/grid.component'
   ],
   imports: [
     BrowserModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatSelectModule,
+    BrowserAnimationsModule
     // MatSliderModule,
     // MatButtonToggleModule,
     // MatGridListModule,
-    // BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.css']
 })
+
+
 export class TopbarComponent implements OnInit {
 
   constructor() { }
