c7eafe1457ff4573702f5bb57677643cdd93ae71
Justin-liu-imperial <61479470+Justin-liu-imperial@users.noreply.github.com>
2020-07-07 00:00:32 +0800

    Created point components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,24 @@ import { AppComponent } from './app.component';
 // import { MatSliderModule } from '@angular/material/slider';
 // import { MatButtonToggleModule } from '@angular/material/button-toggle';
 // import { MatGridListModule } from '@angular/material/grid-list';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopbarComponent } from './topbar/topbar.component';
-import { GridComponent } from './grid/grid.component'
+import { GridComponent } from './grid/grid.component';
+import { PointComponent } from './point/point.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    GridComponent
+    GridComponent,
+    PointComponent
   ],
   imports: [
     BrowserModule,
     // MatSliderModule,
     // MatButtonToggleModule,
     // MatGridListModule,
-    // BrowserAnimationsModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
