a6343d833931c7f606d5a567a8c6b3fa5a76e842
Aron Teh <ewt17@ic.ac.uk>
2020-07-11 23:37:03 +0100

    Tasks performed in this branch: - Edit the path info bold text, add separator √ - Remove the bubble on top of initialise vertices √ - Corrected min and max allowed nodes √ - Randomise button seems to be off alignment, CSS √ - Slow-Fast slider is changing width √ - Node slider changes length while changing the node √ - Change max node to 50 for now √ - Slightly changed margin of grid component (let me know if this does anything unintended to your side) √ - iPad interface requires scrolling even though there's space √

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -3,7 +3,6 @@ import {FormControl} from '@angular/forms';
 import { GridcommService } from '../gridcomm.service';
 
 
-
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
@@ -18,18 +17,18 @@ export class TopbarComponent implements OnInit, DoCheck {
   selectedPoints: {x: number, y:number}[] = []; //array of coordinate objects to store only selected points
   prevSelectedPoints: {x: number, y:number}[] = []; //array with previous selected points for change detection
 
-  noRows = 21; //number of rows of points
-  noCols = 50; //number of columns of points
+  noRows = 21;  // Number of rows of points
+  noCols = 50;  // Number of columns of points
   currPointAmount = 0;
   randomPointAmount: number = 2; //start at min number of points
-  possPaths: string = '0'; //possible number of paths - string so we can format it better?
+  possPaths: string = '0';  // Possible number of paths - string so we can format it better?
 
   ngOnInit(): void {
     this.data.currentSelPointsMessage.subscribe(message => this.selectedPoints = message); //subscribe to selectedpoints 'message' - accesses the selectedpoints array held in the service; any updates will immediately be pushed to this.selectedPoints
     this.setPossiblePaths();
   }
 
-  ngDoCheck() { //runs whenever a change is detected in ANY component in the app (could be mouse changes or any data changes)
+  ngDoCheck() {  // Runs whenever a change is detected in ANY component in the app (could be mouse changes or any data changes)
     if(!this.arraysEqual(this.prevSelectedPoints, this.selectedPoints)){ //if there is a change in selected points
       this.prevSelectedPoints = this.selectedPoints.slice(0); //update previous selected points; slice makes copy of array - cannot just update with (=)
       this.currPointAmount = this.selectedPoints.length;
@@ -42,7 +41,7 @@ export class TopbarComponent implements OnInit, DoCheck {
     this.setPossiblePaths();
   }
 
-  randomize(no: number): void{ //auxiliary function (can be called from html) to add a number (no) of random points in the grid - combine with clearAll for generation instead of appending random points
+  randomize(no: number): void{  // Auxiliary function (can be called from html) to add a number (no) of random points in the grid - combine with clearAll for generation instead of appending random points
     this.clearAll();
     for(let num = 0; num < this.randomPointAmount; num++){
       var added = false;
@@ -52,27 +51,24 @@ export class TopbarComponent implements OnInit, DoCheck {
     }
   }
 
-  clearAll(): void{ //auxiliary function (can be called from html) to clear all selected points
-    this.data.changeSelPointMessage([]); //empties the selectedpoints 'message' - see gridcomm.service.ts for more information
+  clearAll(): void{  // Auxiliary function (can be called from html) to clear all selected points
+    this.data.changeSelPointMessage([]);  // Empties the selectedpoints 'message' - see gridcomm.service.ts for more information
   }
-
 
   setPossiblePaths(): void{
     this.possPaths = this.currPointAmount.toString();
   }
 
-
-
-  getRandomNumberBetweenZ(max: number){ //helper function to generate random number
+  getRandomNumberBetweenZ(max: number){  // Helper function to generate random number
     return Math.floor(Math.random()*(max));
   }
 
-  arraysEqual(a: any[], b: any[]) { //array equality checker (can't simply call [] === [] in typescript)
+  arraysEqual(a: any[], b: any[]) {  // Array equality checker (can't simply call [] === [] in typescript)
     if (a === b) return true;
     if (a == null || b == null) return false;
     if (a.length !== b.length) return false;
 
-    for (var i = 0; i < a.length; ++i) { //element-wise checking
+    for (var i = 0; i < a.length; ++i) {  // Element-wise checking
       if (a[i] !== b[i]) return false;
     }
     return true;
