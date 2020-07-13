07d8b057d78fccc0caced07f9f95e135e444e594
Aron Teh <ewt17@ic.ac.uk>
2020-07-13 21:56:36 +0100

    Implemented stopwatch (start, stop, pause) with changing button colours

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { GridcommService } from '../gridcomm.service';
+import { timer } from 'rxjs';
 
 
 @Component({
@@ -11,26 +12,38 @@ import { GridcommService } from '../gridcomm.service';
 
 export class TopbarComponent implements OnInit, DoCheck {
 
+  // Emptry constructor for now
   constructor(private data: GridcommService) { }
 
-  selectedPoints: {x: number, y:number}[] = []; //array of coordinate objects to store only selected points
-  prevSelectedPoints: {x: number, y:number}[] = []; //array with previous selected points for change detection
+  selectedPoints: {x: number, y:number}[] = [];     // Array of coordinate objects to store only selected points
+  prevSelectedPoints: {x: number, y:number}[] = []; // Array with previous selected points for change detection
 
   noRows = 21;  // Number of rows of points
   noCols = 50;  // Number of columns of points
 
-  currPointAmount:number = 0;
-  randomPointAmount: number = 3; // Start at min number of points
-  possPaths:string = "0";  // Possible number of paths - string so we can format it better?
+  currPointAmount:number = 0;     // Number of current points
+  randomPointAmount: number = 3;  // Start at min number of points
+  possPaths:string = "0";         // Possible number of paths
 
+  counter:number = 0;                   // Timer counter
+  counterSeconds:number = 0;            // Timer counter in seconds
+  timerRunning:boolean = false;         // Boolean to know if timer is running
+  startText:string = "Start!";          // Text at beginning
+  timeRef;                              // Reference time from start time
+  startButtonColor:string = "success";  // Button color
+
+  // Subscribe to selectedpoints 'message' - accesses the selectedpoints array held in the service;
+  // Any updates will immediately be pushed to this.selectedPoints
   ngOnInit(): void {
-    this.data.currentSelPointsMessage.subscribe(message => this.selectedPoints = message); //subscribe to selectedpoints 'message' - accesses the selectedpoints array held in the service; any updates will immediately be pushed to this.selectedPoints
+    this.data.currentSelPointsMessage.subscribe(message => this.selectedPoints = message);
     this.possPaths = this.calculatePossiblePaths(this.currPointAmount);
   }
 
-  ngDoCheck(): void {  // Runs whenever a change is detected in ANY component in the app (could be mouse changes or any data changes)
+  // Runs whenever a change is detected in ANY component in the app (could be mouse changes or any data changes)
+  ngDoCheck(): void {
     if(!this.arraysEqual(this.prevSelectedPoints, this.selectedPoints)) {  // If there is a change in selected points
-      this.prevSelectedPoints = this.selectedPoints.slice(0); //update previous selected points; slice makes copy of array - cannot just update with (=)
+      // Update previous selected points; slice makes copy of array - cannot just update with (=)
+      this.prevSelectedPoints = this.selectedPoints.slice(0);
       this.currPointAmount = this.selectedPoints.length;
       this.possPaths = this.calculatePossiblePaths(this.currPointAmount);
     }
@@ -38,6 +51,7 @@ export class TopbarComponent implements OnInit, DoCheck {
 
   // Calculate the possible number of paths
   calculatePossiblePaths(currPointAmount:number): string{
+    // Format properly depending on number size
     let possiblePaths = this.factorial(currPointAmount - 1) / 2
     if (possiblePaths < 100000000) {
       return possiblePaths.toString()
@@ -52,30 +66,39 @@ export class TopbarComponent implements OnInit, DoCheck {
     return (currPointAmount != 1) ? currPointAmount * this.factorial(currPointAmount - 1) : 1;
   }
 
+  // Function to update the number of random points selected
   updateRandomAmount(event): void {
     this.randomPointAmount = event.value;
-    this.possPaths = this.calculatePossiblePaths(this.currPointAmount);
+    this.possPaths = this.calculatePossiblePaths(this.currPointAmount);  // Recalculate possible paths
   }
 
-  randomize(no: number): void{  // Auxiliary function (can be called from html) to add a number (no) of random points in the grid - combine with clearAll for generation instead of appending random points
+  // Auxiliary function (can be called from html) to add a number (no) of random points in the grid
+  // Combine with clearAll for generation instead of appending random points
+  randomize(no: number): void{
     this.clearAll();
     for(let num = 0; num < no; num++){
       var added = false;
       while(!added){
-        added = this.data.addToSelPointsMessage({x: this.getRandomNumberBetweenZ(this.noCols), y: this.getRandomNumberBetweenZ(this.noRows)}); //updates the selectedpoints 'message' with the new random point - see gridcomm.service.ts for more information
+        // Updates the selectedpoints 'message' with the new random point. More info: gridcomm.service.ts
+        added = this.data.addToSelPointsMessage(
+          { x: this.getRandomNumberBetweenZ(this.noCols),
+            y: this.getRandomNumberBetweenZ(this.noRows)});
       }
     }
   }
 
-  clearAll(): void{  // Auxiliary function (can be called from html) to clear all selected points
-    this.data.changeSelPointMessage([]);  // Empties the selectedpoints 'message' - see gridcomm.service.ts for more information
+  // Auxiliary function (can be called from html) to clear all selected points
+  clearAll(): void{
+    this.data.changeSelPointMessage([]);  // Empties the selectedpoints 'message'; More info: gridcomm.service.ts
   }
 
-  getRandomNumberBetweenZ(max: number): number{  // Helper function to generate random number
+  // Helper function to generate random number
+  getRandomNumberBetweenZ(max: number): number{
     return Math.floor(Math.random()*(max));
   }
 
-  arraysEqual(a: any[], b: any[]): boolean {  // Array equality checker (can't simply call [] === [] in typescript)
+  // Array equality checker (can't simply call [] === [] in typescript)
+  arraysEqual(a: any[], b: any[]): boolean {
     if (a === b) return true;
     if (a == null || b == null) return false;
     if (a.length !== b.length) return false;
@@ -84,6 +107,34 @@ export class TopbarComponent implements OnInit, DoCheck {
       if (a[i] !== b[i]) return false;
     }
     return true;
+  }
+
+  // Start timer
+  startTimer(): void {
+    this.timerRunning = !this.timerRunning  // Negate whether timer is running
+    if (this.timerRunning) {
+      this.startText = "Pause";
+      this.startButtonColor = "accent";
+      const startTime = Date.now() - this.counter;
+      this.timeRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+        this.counterSeconds = Math.round(this.counter/1000);
+      });
+    }
+    else {
+      this.startText = "Start!";
+      this.startButtonColor = "success";
+      clearInterval(this.timeRef);
+    }
+  }
+
+  // Reset timer
+  resetTimer(): void {
+    this.timerRunning = false;
+    this.startText = "Start!";
+    this.counter = 0;
+    this.counterSeconds = 0;
+    clearInterval(this.timeRef);
   }
 
 }
