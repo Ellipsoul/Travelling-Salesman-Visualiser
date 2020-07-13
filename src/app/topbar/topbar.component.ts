118ffd30a6852de8ad3f874b29cd2f785ae19456
Aron Teh <ewt17@ic.ac.uk>
2020-07-13 20:42:31 +0100

    Implemented function for calculating total possible paths

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -13,31 +13,48 @@ export class TopbarComponent implements OnInit, DoCheck {
 
   constructor(private data: GridcommService) { }
 
-
   selectedPoints: {x: number, y:number}[] = []; //array of coordinate objects to store only selected points
   prevSelectedPoints: {x: number, y:number}[] = []; //array with previous selected points for change detection
 
   noRows = 21;  // Number of rows of points
   noCols = 50;  // Number of columns of points
-  currPointAmount = 0;
+
+  currPointAmount:number = 0;
   randomPointAmount: number = 2; //start at min number of points
-  possPaths: string = '0';  // Possible number of paths - string so we can format it better?
+  possPaths:string = "0";  // Possible number of paths - string so we can format it better?
 
   ngOnInit(): void {
     this.data.currentSelPointsMessage.subscribe(message => this.selectedPoints = message); //subscribe to selectedpoints 'message' - accesses the selectedpoints array held in the service; any updates will immediately be pushed to this.selectedPoints
-    this.setPossiblePaths();
+    this.possPaths = this.calculatePossiblePaths(this.currPointAmount);
   }
 
   ngDoCheck(): void {  // Runs whenever a change is detected in ANY component in the app (could be mouse changes or any data changes)
-    if(!this.arraysEqual(this.prevSelectedPoints, this.selectedPoints)){ //if there is a change in selected points
+    if(!this.arraysEqual(this.prevSelectedPoints, this.selectedPoints)) {  // If there is a change in selected points
       this.prevSelectedPoints = this.selectedPoints.slice(0); //update previous selected points; slice makes copy of array - cannot just update with (=)
       this.currPointAmount = this.selectedPoints.length;
+      this.possPaths = this.calculatePossiblePaths(this.currPointAmount);
     }
+  }
+
+  // Calculate the possible number of paths
+  calculatePossiblePaths(currPointAmount:number): string{
+    let possiblePaths = this.factorial(currPointAmount) / 2
+    if (possiblePaths < 100000000) {
+      return possiblePaths.toString()
+    }
+    else {
+      return possiblePaths.toExponential(3)
+    }
+  }
+
+  // Simple factorial
+  factorial(currPointAmount:number): number {
+    return (currPointAmount != 1) ? currPointAmount * this.factorial(currPointAmount - 1) : 1;
   }
 
   updateRandomAmount(event): void {
     this.randomPointAmount = event.value;
-    this.setPossiblePaths();
+    this.possPaths = this.calculatePossiblePaths(this.currPointAmount);
   }
 
   randomize(no: number): void{  // Auxiliary function (can be called from html) to add a number (no) of random points in the grid - combine with clearAll for generation instead of appending random points
@@ -52,10 +69,6 @@ export class TopbarComponent implements OnInit, DoCheck {
 
   clearAll(): void{  // Auxiliary function (can be called from html) to clear all selected points
     this.data.changeSelPointMessage([]);  // Empties the selectedpoints 'message' - see gridcomm.service.ts for more information
-  }
-
-  setPossiblePaths(): void{
-    this.possPaths = this.currPointAmount.toString();
   }
 
   getRandomNumberBetweenZ(max: number): number{  // Helper function to generate random number
