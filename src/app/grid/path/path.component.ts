7d85e9a062612cda65ec88ce2ea9e13a540ec2ad
Aron Teh <ewt17@ic.ac.uk>
2020-07-15 21:38:44 +0100

    Not much progress with improving lines

--- src/app/grid/path/path.component.ts
+++ src/app/grid/path/path.component.ts
@@ -4,62 +4,64 @@ import { MinLengthValidator } from '@angular/forms';
 @Component({
   selector: 'app-path',
   templateUrl: './path.component.html',
-  styleUrls: ['./path.component.css']
+  styleUrls: ['./path.component.css'],
 })
-
 export class PathComponent implements OnInit {
-  pointA: {x: number; y:number};
-  pointB: {x: number; y:number};
+  pointA: { x: number; y: number };
+  pointB: { x: number; y: number };
 
-  type: number = 0; //0 - vertical; 1 - horizontal, 2 - bot-left to top-right; 3 - top-left to bot-right
-
+  // 0 - vertical; 1 - horizontal, 2 - bot-left to top-right; 3 - top-left to bot-right
+  type: number = 0;
   pointSpacing: number = 34;
 
-  pathWidth: number = 50; //div width
-  pathHeight: number = 50; //divheight
-  pathLeft: number = 30+12.5; //div offset from left
-  pathTop: number = 12.5; //div offset from top
+  pathWidth: number = 50; // Div width
+  pathHeight: number = 50; // Div height
+  pathLeft: number = 30 + 12.5; // Div offset from left
+  pathTop: number = 12.5; // Div offset from top
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
-
-  setPath(inPath: {A:{x: number; y:number}; B: {x: number; y:number}}): void{
+  setPath(inPath: {
+    A: { x: number; y: number };
+    B: { x: number; y: number };
+  }): void {
     this.pointA = inPath.A;
     this.pointB = inPath.B;
     var xdiff = this.pointA.x - this.pointB.x;
     var ydiff = this.pointA.y - this.pointB.y;
-    var xmin = Math.min(this.pointA.x,this.pointB.x);
-    var ymin = Math.min(this.pointA.y,this.pointB.y);
+    var xmin = Math.min(this.pointA.x, this.pointB.x);
+    var ymin = Math.min(this.pointA.y, this.pointB.y);
 
-    if(xdiff == 0){ //vertical line
+    if (xdiff == 0) {
+      // Vertical line
       this.type = 0;
       this.pathWidth = 5;
-      this.pathHeight = Math.abs(ydiff)*this.pointSpacing;
-      this.pathLeft = 24.5+this.pointSpacing/2 + xmin*this.pointSpacing;
-      this.pathTop =  this.pointSpacing/2 + ymin*this.pointSpacing;
-    }else if(ydiff == 0){ //horizontal line
+      this.pathHeight = Math.abs(ydiff) * this.pointSpacing;
+      this.pathLeft = 24.5 + this.pointSpacing / 2 + xmin * this.pointSpacing;
+      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
+    } else if (ydiff == 0) {
+      // Horizontal line
       this.type = 1;
-      this.pathWidth = Math.abs(xdiff)*this.pointSpacing;
+      this.pathWidth = Math.abs(xdiff) * this.pointSpacing;
       this.pathHeight = 5;
-      this.pathLeft = 30+this.pointSpacing/2 + xmin*this.pointSpacing;
-      this.pathTop =  this.pointSpacing/2 + ymin*this.pointSpacing;
-    }else if(xdiff/ydiff < 0){ //bot-left to top-right
+      this.pathLeft = 30 + this.pointSpacing / 2 + xmin * this.pointSpacing;
+      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
+    } else if (xdiff / ydiff < 0) {
+      // Bot-left to top-right
       this.type = 2;
-      this.pathWidth = Math.abs(xdiff)*this.pointSpacing;
-      this.pathHeight = Math.abs(ydiff)*this.pointSpacing;
-      this.pathLeft = 30+this.pointSpacing/2 + xmin*this.pointSpacing;
-      this.pathTop =  this.pointSpacing/2 + ymin*this.pointSpacing;
-    }else{ //top-left to bot-right
+      this.pathWidth = Math.abs(xdiff) * this.pointSpacing;
+      this.pathHeight = Math.abs(ydiff) * this.pointSpacing;
+      this.pathLeft = 30 + this.pointSpacing / 2 + xmin * this.pointSpacing;
+      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
+    } else {
+      // Top-left to bot-right
       this.type = 3;
-      this.pathWidth = Math.abs(xdiff)*this.pointSpacing;
-      this.pathHeight = Math.abs(ydiff)*this.pointSpacing;
-      this.pathLeft = 30+this.pointSpacing/2 + xmin*this.pointSpacing;
-      this.pathTop =  this.pointSpacing/2 + ymin*this.pointSpacing;
+      this.pathWidth = Math.abs(xdiff) * this.pointSpacing;
+      this.pathHeight = Math.abs(ydiff) * this.pointSpacing;
+      this.pathLeft = 30 + this.pointSpacing / 2 + xmin * this.pointSpacing;
+      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
     }
-    // console.log(this.pointA,this.pointB);
-
   }
 }
