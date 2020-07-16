0fe87b6ee84a0428dd5a2f7f65b1accb7b7163f7
Justin-liu-imperial <61479470+Justin-liu-imperial@users.noreply.github.com>
2020-07-16 15:56:49 +0800

    Better paths!

--- src/app/grid/path/path.component.ts
+++ src/app/grid/path/path.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MinLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-path',
@@ -11,13 +10,14 @@ export class PathComponent implements OnInit {
   pointB: { x: number; y: number };
 
   // 0 - vertical; 1 - horizontal, 2 - bot-left to top-right; 3 - top-left to bot-right
-  type: number = 0;
+  type: number = 0; //later on for setting colour
   pointSpacing: number = 34;
 
   pathWidth: number = 50; // Div width
   pathHeight: number = 50; // Div height
   pathLeft: number = 30 + 12.5; // Div offset from left
   pathTop: number = 12.5; // Div offset from top
+  rotation: number = 0; // Div rotation from top left
 
   constructor() {}
 
@@ -31,37 +31,12 @@ export class PathComponent implements OnInit {
     this.pointB = inPath.B;
     var xdiff = this.pointA.x - this.pointB.x;
     var ydiff = this.pointA.y - this.pointB.y;
-    var xmin = Math.min(this.pointA.x, this.pointB.x);
-    var ymin = Math.min(this.pointA.y, this.pointB.y);
+    var dist = Math.hypot(xdiff,ydiff);
 
-    if (xdiff == 0) {
-      // Vertical line
-      this.type = 0;
-      this.pathWidth = 5;
-      this.pathHeight = Math.abs(ydiff) * this.pointSpacing;
-      this.pathLeft = 27 + this.pointSpacing / 2 + xmin * this.pointSpacing;
-      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
-    } else if (ydiff == 0) {
-      // Horizontal line
-      this.type = 1;
-      this.pathWidth = Math.abs(xdiff) * this.pointSpacing;
-      this.pathHeight = 5;
-      this.pathLeft = 30 + this.pointSpacing / 2 + xmin * this.pointSpacing;
-      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing - 2.7;
-    } else if (xdiff / ydiff < 0) {
-      // Bot-left to top-right
-      this.type = 2;
-      this.pathWidth = Math.abs(xdiff) * this.pointSpacing;
-      this.pathHeight = Math.abs(ydiff) * this.pointSpacing;
-      this.pathLeft = 30 + this.pointSpacing / 2 + xmin * this.pointSpacing;
-      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
-    } else {
-      // Top-left to bot-right
-      this.type = 3;
-      this.pathWidth = Math.abs(xdiff) * this.pointSpacing;
-      this.pathHeight = Math.abs(ydiff) * this.pointSpacing;
-      this.pathLeft = 30 + this.pointSpacing / 2 + xmin * this.pointSpacing;
-      this.pathTop = this.pointSpacing / 2 + ymin * this.pointSpacing;
-    }
+    this.pathWidth = dist * this.pointSpacing;
+    this.pathHeight = 10;
+    this.pathTop = this.pointSpacing / 2 + inPath.A.y * this.pointSpacing;
+    this.pathLeft = this.pointSpacing / 2 + inPath.A.x * this.pointSpacing;
+    this.rotation = -Math.atan2(ydiff,-xdiff)*180/Math.PI;
   }
 }
